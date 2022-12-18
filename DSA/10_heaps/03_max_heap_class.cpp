0e3a4d089307c798d2c99a67383455dcd9588b51
MR-DHRUV <dhruvgupta742@gmail.com>
2022-12-18 23:08:53 +0530

    18-12-22

--- DSA/10_heaps/03_max_heap_class.cpp
+++ DSA/10_heaps/03_max_heap_class.cpp
@@ -6,23 +6,43 @@ class MaxHeap
     int *h;
     int size;
     int len;
+    int maxLen;
 
 public:
     MaxHeap(){};
     MaxHeap(int x) : size(x), len(-1)
     {
+        maxLen = len;
         h = new int[size];
     };
 
     void create(vector<int> input);
     void insert(int x);
+    void del();
 
     void print();
+    void printArr();
 };
 
 void MaxHeap ::print()
 {
+    if (len == -1)
+    {
+        cout << "Heap is empty" << endl;
+        return;
+    }
+
     for (int i = 0; i <= len; i++)
+    {
+        cout << h[i] << " ";
+    }
+
+    cout << endl;
+}
+
+void MaxHeap ::printArr()
+{
+    for (int i = 0; i <= maxLen; i++)
     {
         cout << h[i] << " ";
     }
@@ -40,6 +60,7 @@ void MaxHeap::insert(int x)
     }
 
     h[++len] = x;
+    maxLen = len;
 
     int i = len;
 
@@ -70,7 +91,8 @@ void MaxHeap ::create(vector<int> input)
     if (size == 0)
     {
         len = -1;
-        size = 2 * input.size();
+        maxLen = len;
+        size = input.size();
         h = new int[size];
     }
 
@@ -81,6 +103,114 @@ void MaxHeap ::create(vector<int> input)
     {
         insert(input[i]);
     }
+}
+
+void MaxHeap ::del()
+{
+
+    if (len < 1)
+    {
+        len = -1;
+        return;
+    }
+
+    // We can only delete root element from a heap
+    // that means we can only delete highest priority element
+
+    /*
+            30
+          /   \
+        20     15
+       / \    /  \
+      5  10  12   6
+
+    */
+
+    // When we delete root our heap will look like this
+
+    /*
+             20
+           /   \
+         5     15
+          \    /  \
+          10  12   6
+
+   */
+
+    // problem
+    // 1 Inpomplete tree, thus empty space , so invalid heap
+    // 2 not every element follows the property of max heap
+
+    // solution
+    // put last element in place of root
+    // then compare children of new root with new root and swap new root with max value and keep on doing it
+
+    // 1
+    /*
+            6
+          /   \
+        20     15
+       / \    /
+      5  10  12
+
+    */
+
+    // 2
+    /*
+           20
+          /   \
+        6     15
+       / \    /
+      5  10  12
+
+    */
+
+    /*
+           20
+          /   \
+        10     15
+       / \    /
+      5   6  12
+
+    */
+
+    // step 1
+    int removedElement = h[0];
+    h[0] = h[len--];
+
+    // step 2
+    int i = 0;           // on new root
+    int j = (2 * i) + 1; // on root's left child, +1 as our index starts from 0
+
+    // we have to repeat this swapping until a node has no child that is j is out of bound
+    while (j <= len)
+    {
+        if (h[j + 1] > h[j])
+        {
+            j++;
+        }
+
+        // child is greater
+        if (h[i] < h[j])
+        {
+            swap(h[i], h[j]);
+
+            // now we have corrected the above tree and now we have to correct the lower tree so bringing i to swapped  element and j again to its left child
+            i = j;
+            j = (2 * i) + 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    // concides the fact
+    // it we start storing deleted elements at last indices , then we will have sorted array and this is called heap sort
+    // they will be in decending order
+
+    // storing that element at last index
+    h[len + 1] = removedElement;
 }
 
 int main()
@@ -95,13 +225,23 @@ int main()
 
     // mh.print();
 
-
     MaxHeap mh;
 
-    // order of elements in heap is independent of order of insertion if elements are same 
-    vector<int> elements = {1,2,3,4,4,5,6,8};
+    // order of elements in heap is independent of order of insertion if elements are same
+    vector<int> elements = {1, 2, 3, 4, 4, 5, 6, 8};
     mh.create(elements);
     mh.print();
+
+    mh.del();
+    mh.printArr();
+    mh.print();
+
+    mh.del();
+    mh.printArr();
+    mh.print();
+
+
+
 
     return 0;
 }
---
+++ DSA/10_heaps/04_heap_sort.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    // heap sort
+    
+    // 1 create heap of n elements  // O(NlogN)
+    // 2 delete all the n elements  // O(NlogN)
+
+
+    // complexity 
+    // time : O(NlogN)
+    // space : O(N)
+
+    return 0;
+}   
