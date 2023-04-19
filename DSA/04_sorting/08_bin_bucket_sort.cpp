b4da7d3b9a62b08151de5e3209ca45410ee79196
MR-DHRUV <dhruvgupta742@gmail.com.com>
2023-04-19 15:37:52 +0530

    19-4-23

---
+++ DSA/04_sorting/08_bin_bucket_sort.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    // bucket/bin sort
+    // similar to count sort
+    // here we have bins/ linked list in count array and whenever we enconter any element we add it to the bin by inserting that element in the linkedlist of that index
+
+    // useless as high space complexity 
+    // bin baat linked list kyu banani haii
+
+    
+
+    return 0;
+}
