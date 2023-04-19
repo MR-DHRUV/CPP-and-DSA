84a3e45008733af27766be267fdf8bcfc94ad410
MR-DHRUV <dhruvgupta742@gmail.com>
2023-04-19 13:17:08 +0530

    19-4-23

---
+++ DSA/04_sorting/07_count_sort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+unsigned long* CountingSort(unsigned long data[], long Size)
+{
+    unsigned long maxEle = 0;
+
+    for (int i = 0; i < Size; i++)
+    {
+        maxEle = max(maxEle,data[i]);
+    }
+
+    
+    
+    
+}
+
+int main()
+{
+    // ginti krko konsa kitni baar haii 
+    // O(N) time but space = O(max element)
+
+    // if array is like 1 2 1000000
+    // space used will be 1000000
+
+
+    return 0;
+}
